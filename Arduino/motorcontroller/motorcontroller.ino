934c5467835293ac938619eef6f4ee41675b7b08
Alan <alansuki@gmail.com>
2015-03-04 14:14:32 -0600

    Adds definitions for quad motor control

--- Arduino/motorcontroller/motorcontroller.ino
+++ Arduino/motorcontroller/motorcontroller.ino
@@ -1,18 +1,30 @@
 /*
   Define pin assignments
 */
-#define pinA1 5
-#define pinA2 7
-#define pinA3 8
-#define pinA4 9
+#define L_A0 5
+#define L_A1 7
+#define L_B0 8
+#define L_B1 9
+#define R_A0 5
+#define R_A1 7
+#define R_B0 8
+#define R_B1 9
 
 #define led 13
 
 // Individual motor motions definitions
-#define FRONT_LEFT_FORWARD(x) analogWrite(pinA1,map(x,0,100,0,255));digitalWrite(pinA2,LOW);
-#define FRONT_LEFT_BACKWARD(x) analogWrite(pinA1,map(x,0,100,255,0));digitalWrite(pinA2,HIGH);
-#define FRONT_RIGHT_FORWARD(x) analogWrite(pinA3,map(x,0,100,0,255));digitalWrite(pinA4,LOW);
-#define FRONT_RIGHT_BACKWARD(x) analogWrite(pinA3,map(x,0,100,255,0));digitalWrite(pinA4,HIGH);
+#define FRONT_LEFT_FORWARD(x) analogWrite(L_A0,map(x,0,100,0,255));digitalWrite(L_A1,LOW);
+#define FRONT_LEFT_BACKWARD(x) analogWrite(L_A0,map(x,0,100,255,0));digitalWrite(L_A1,HIGH);
+
+#define FRONT_RIGHT_FORWARD(x) analogWrite(R_A0,map(x,0,100,0,255));digitalWrite(R_A1,LOW);
+#define FRONT_RIGHT_BACKWARD(x) analogWrite(R_A0,map(x,0,100,255,0));digitalWrite(R_A1,HIGH);
+
+#define BACK_LEFT_FORWARD(x) analogWrite(L_B0,map(x,0,100,0,255));digitalWrite(L_B1,LOW);
+#define BACK_LEFT_BACKWARD(x) analogWrite(L_B0,map(x,0,100,255,0));digitalWrite(L_B1,HIGH);
+
+#define BACK_RIGHT_FORWARD(x) analogWrite(R_B0,map(x,0,100,0,255));digitalWrite(R_B1,LOW);
+#define BACK_RIGHT_BACKWARD(x) analogWrite(R_B0,map(x,0,100,255,0));digitalWrite(R_B1,HIGH);
+
 
 
 /*
@@ -47,11 +59,17 @@ void rotateRight(int vel)
 
 void setup(){
   // A control outputs for the motor controller
-  // A1-A3 used as PWM outputs, A2-A4 digital
-  pinMode(pinA1, OUTPUT);
-  pinMode(pinA2, OUTPUT);
-  pinMode(pinA3, OUTPUT);
-  pinMode(pinA4, OUTPUT);
+  // A0-B0 used as PWM outputs, A1-B1 digital
+  pinMode(L_A0, OUTPUT);
+  pinMode(L_A1, OUTPUT);
+  pinMode(L_B0, OUTPUT);
+  pinMode(L_B1, OUTPUT);
+  
+  pinMode(R_A0, OUTPUT);
+  pinMode(R_A1, OUTPUT);
+  pinMode(R_B0, OUTPUT);
+  pinMode(R_B1, OUTPUT);
+  
   pinMode(led, OUTPUT);  //status LED
   
   
@@ -66,8 +84,6 @@ forward(100);
 delay(3000);
 backward(100);
 delay(3000);
- 
-  
 
 
 }
